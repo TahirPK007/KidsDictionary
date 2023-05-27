4ac0f67ceac2c6be8604928959bf5287c943ec9a
Tahir Mehmood <tahir.mehmood9425@gmail.com>
2023-05-27 23:53:19 +0500

    implemented logic for mulitple checkboxes with childid

--- src/screen/AssignWordsScreen.js
+++ src/screen/AssignWordsScreen.js
@@ -14,10 +14,13 @@ import {openDatabase} from 'react-native-sqlite-storage';
 const db = openDatabase({name: 'kidsdictionary'});
 
 const AssignWordsScreen = ({route, navigation}) => {
+  const [selectedItems, setSelectedItems] = useState([]);
+
   const childid = route.params.paramkey.id;
   console.log(childid, 'this si child id');
   const [wordsdata, setwordsdata] = useState([]);
   console.log(wordsdata, 'this is wordsssss');
+
   const fetchwords = () => {
     db.transaction(txn => {
       txn.executeSql(
@@ -51,77 +54,57 @@ const AssignWordsScreen = ({route, navigation}) => {
     fetchwords();
   }, []);
 
-  const [checked1, setchecked1] = useState([]);
+  let anotherArray = [];
 
-  const [checkboxItems, setCheckboxItems] = useState([
-    {id: 1, label: 'Random  :  3', checked: false},
-    {id: 2, label: 'School  :  2', checked: false},
-    {id: 3, label: 'Market  :  3', checked: false},
-    {id: 4, label: 'Tennis  :  1', checked: false},
-    {id: 5, label: 'Nation  :  5', checked: false},
-    {id: 6, label: 'Reason  :  3', checked: false},
-    {id: 7, label: 'Appear  :  6', checked: false},
-    {id: 8, label: 'Change  :  1', checked: false},
-    {id: 9, label: 'Fabric  :  5', checked: false},
-    {id: 10, label: 'Packed  :  7', checked: false},
-    {id: 11, label: 'Almost  :  6', checked: false},
-    {id: 12, label: 'Branch  :  2', checked: false},
-    {id: 13, label: 'Qiblah  :  4', checked: false},
-    {id: 14, label: 'Vacuum  :  6', checked: false},
-    {id: 15, label: 'Zagged  :  7', checked: false},
-    {id: 16, label: 'Earbud  :  5', checked: false},
-    {id: 17, label: 'Fabric  :  3', checked: false},
-    {id: 18, label: 'Belief  :  7', checked: false},
-    {id: 19, label: 'Choose  :  4', checked: false},
-    {id: 20, label: 'Flower  :  1', checked: false},
-    {id: 21, label: 'Hammer  :  3', checked: false},
-    {id: 22, label: 'Hacked  :  7', checked: false},
-    {id: 23, label: 'Energy  :  2', checked: false},
-    {id: 24, label: 'Labour  :  4', checked: false},
-    {id: 25, label: 'Define  :  6', checked: false},
-    {id: 26, label: 'Joker  :  1', checked: false},
-    {id: 27, label: 'Macros  :  7', checked: false},
-    {id: 28, label: 'Insect  :  2', checked: false},
-    {id: 29, label: 'Noodle  :  1', checked: false},
-    {id: 30, label: 'Kittie  :  2', checked: false},
-    {id: 31, label: 'Sneaks  :  4', checked: false},
-    {id: 32, label: 'Eraser  :  5', checked: false},
-    {id: 33, label: 'Punish  :  4', checked: false},
-    {id: 34, label: 'Elegant  :  6', checked: false},
-    {id: 35, label: 'Oxygen  :  5', checked: false},
-  ]);
+  // const [checkboxItems, setCheckboxItems] = useState([
+  //   {id: 1, label: 'Random  :  3', checked: false},
+  //   {id: 2, label: 'School  :  2', checked: false},
+  // ]);
 
-  const handleCheckboxToggle = itemId => {
-    const updatedItems = checkboxItems.map(item => {
-      if (item.id === itemId) {
-        return {...item, checked: !item.checked};
-      }
-      return item;
-    });
-    setCheckboxItems(updatedItems);
+  // const handleCheckboxToggle = itemId => {
+  //   const updatedItems = checkboxItems.map(item => {
+  //     if (item.id === itemId) {
+  //       return {...item, checked: !item.checked};
+  //     }
+  //     return item;
+  //   });
+  //   setCheckboxItems(updatedItems);
+  // };
+
+  const handleCheckboxPress = item => {
+    const modifiedItem = {
+      ...item,
+      childId: childid,
+    };
+
+    if (selectedItems.some(selectedItem => selectedItem.id === item.id)) {
+      setSelectedItems(selectedItems.filter(i => i.id !== item.id));
+    } else {
+      setSelectedItems([...selectedItems, modifiedItem]);
+    }
   };
+
+  const existingArray = [];
+
+  anotherArray = existingArray.concat(selectedItems);
+
+  console.log(anotherArray, 'this array contains data');
 
   return (
     <ScrollView style={styles.container1}>
       <Text style={{...styles.title1}}>Assign Word's Screen</Text>
 
       <View>
-        <FlatList
-          data={wordsdata}
-          renderItem={({item, index}) => {
-            return (
-              <View>
-                <CheckBox
-                  key={item.id}
-                  checked={item.checked}
-                  onPress={() => handleCheckboxToggle(item.id)}
-                  title={item.wordname}
-                  checkedColor="seagreen"
-                />
-              </View>
-            );
-          }}
-        />
+        {wordsdata.map(item => (
+          <CheckBox
+            key={item.id}
+            title={item.wordname}
+            checked={selectedItems.some(
+              selectedItem => selectedItem.id === item.id,
+            )}
+            onPress={() => handleCheckboxPress(item)}
+          />
+        ))}
       </View>
 
       <View style={styles.buttonContainer}>
