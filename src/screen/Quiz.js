d65f31a4db5393e11d18aa450581bdb0d5635858
Tahir Mehmood <tahir.mehmood9425@gmail.com>
2023-06-09 23:22:29 +0500

    done quiz

--- src/screen/Quiz.js
+++ src/screen/Quiz.js
@@ -1,4 +1,11 @@
-import {View, Text, Image, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  Alert,
+} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {openDatabase} from 'react-native-sqlite-storage';
 import Video from 'react-native-video';
@@ -10,75 +17,14 @@ const Quiz = ({route, navigation}) => {
   const kidname = route.params.paramkey.name;
 
   const [kidsdata, setkidsdata] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(null);
+  const [selectedOption, setSelectedOption] = useState(null);
 
-  const [questions, setQuestions] = useState([]);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [options, setOptions] = useState([]);
-  const [selectedOption, setSelectedOption] = useState('');
-  const [isAnswered, setIsAnswered] = useState(false);
-  const [isCorrect, setIsCorrect] = useState(false);
-
-  const handleOptionPress = option => {
-    setSelectedOption(option);
-    setIsAnswered(true);
-
-    if (option === questions[currentQuestionIndex].meaning) {
-      setIsCorrect(true);
-    } else {
-      setIsCorrect(false);
-    }
-
-    // Delay moving to the next question for better user experience
-    setTimeout(() => {
-      moveToNextQuestion();
-    }, 1000);
-  };
-
-  const moveToNextQuestion = () => {
-    if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-      setSelectedOption('');
-      setIsAnswered(false);
-      setIsCorrect(false);
-      generateOptions();
-    } else {
-      // Quiz is finished
-      // Do something, like showing the final score
-    }
-  };
-
-  const generateOptions = () => {
-    const currentQuestion = questions[currentQuestionIndex];
-    const allOptions = [currentQuestion.meaning];
-
-    // Generate two random incorrect options
-    while (allOptions.length < 3) {
-      const randomQuestion = questions[randomNumber(0, questions.length - 1)];
-      const randomOption = randomQuestion.meaning;
-
-      if (!allOptions.includes(randomOption)) {
-        allOptions.push(randomOption);
-      }
-    }
-
-    // Shuffle the options
-    const shuffledOptions = allOptions.sort(() => 0.5 - Math.random());
-    setOptions(shuffledOptions);
-  };
+  const [RandomOptions, setRandomOptions] = useState([]);
 
   const randomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
-
-  if (questions.length === 0) {
-    return (
-      <View style={{}}>
-        <Text>Loading questions...</Text>
-      </View>
-    );
-  }
-
-  const currentQuestion = questions[currentQuestionIndex];
 
   const fetchkidwords = childid => {
     db.transaction(txn => {
@@ -100,7 +46,8 @@ const Quiz = ({route, navigation}) => {
             });
           }
           setkidsdata(resultset);
-          console.log('all words are fetched');
+
+          console.log('all quiz wordsss are fetched');
         },
         error => {
           console.log('error occured while fetching employees');
@@ -113,34 +60,94 @@ const Quiz = ({route, navigation}) => {
     fetchkidwords(childid);
   }, []);
 
-  console.log(kidsdata, 'its kids words');
+  useEffect(() => {
+    if (kidsdata.length > 0) {
+      const randomIndex = randomNumber(0, kidsdata.length - 1);
+      const question = kidsdata[randomIndex];
+      setCurrentQuestion(question);
+
+      const options = kidsdata.filter(
+        data => data.wordname !== question.wordname,
+      );
+
+      const randomOptions = [];
+
+      randomOptions.push(question); // Include the correct answer as one of the options
+
+      while (randomOptions.length < 3) {
+        const randomOptionIndex = randomNumber(0, options.length - 1);
+        const randomOption = options[randomOptionIndex];
+        randomOptions.push(randomOption);
+        options.splice(randomOptionIndex, 1);
+      }
+
+      randomOptions.sort(() => Math.random() - 0.5); // Shuffle the options
+      setRandomOptions(randomOptions);
+    }
+  }, [kidsdata]);
+
+  const handleOptionSelect = option => {
+    setSelectedOption(option);
+
+    // Check if the selected option is correct
+    const isCorrect = option === currentQuestion.wordname;
+
+    // Display an alert with the result
+    const alertTitle = isCorrect ? 'Correct Answer!' : 'Incorrect Answer!';
+    const alertMessage = isCorrect
+      ? 'Congratulations, you selected the correct answer.'
+      : `Oops, the selected answer is incorrect. The correct answer is ${currentQuestion.wordname}.`;
+
+    Alert.alert(
+      alertTitle,
+      alertMessage,
+      [
+        {
+          text: 'Next',
+          onPress: () => {
+            // Fetch a new random question
+            if (kidsdata.length > 1) {
+              const remainingQuestions = kidsdata.filter(
+                data => data.id !== currentQuestion.id,
+              );
+              const randomIndex = randomNumber(
+                0,
+                remainingQuestions.length - 1,
+              );
+              setCurrentQuestion(remainingQuestions[randomIndex]);
+              setkidsdata(remainingQuestions);
+            } else {
+              setCurrentQuestion(null);
+              setkidsdata([]);
+              console.log('Quiz completed!');
+            }
+          },
+        },
+      ],
+      {cancelable: false},
+    );
+  };
+
+  console.log(kidsdata, 'its quizz kids words');
   return (
     <View style={{flex: 1}}>
-      <Image
-        source={{uri: currentQuestion.image}}
-        style={{height: 100, width: 100}}
-      />
-
-      <Text style={{}}>{currentQuestion.wordname}</Text>
-
-      <View style={{}}>
-        {kidsdata.map((option, index) => (
-          <TouchableOpacity
-            key={index}
-            style={[
-              styles.optionButton,
-              selectedOption === option && isAnswered && isCorrect
-                ? styles.correctOption
-                : selectedOption === option && isAnswered && !isCorrect
-                ? styles.incorrectOption
-                : null,
-            ]}
-            disabled={isAnswered}
-            onPress={() => handleOptionPress(option)}>
-            <Text style={styles.optionText}>{option}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
+      <Text>quiz screen</Text>
+      {currentQuestion && (
+        <View>
+          <Image
+            source={{uri: currentQuestion.image}}
+            style={{width: 200, height: 200}}
+          />
+          <Text>Options:</Text>
+          {kidsdata.map(data => (
+            <TouchableOpacity
+              key={data.id}
+              onPress={() => handleOptionSelect(data.wordname)}>
+              <Text>{data.wordname}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      )}
     </View>
   );
 };
